b87a493c9abd55ae66217835fd288fff51e997b7
Khan <mpnkhan@gmail.com>
2019-09-05 15:07:58 +0530

    Add ReadMe

---
+++ README.md
@@ -0,0 +1,7 @@
+# lightcrawler
+Runner accessibility Audits by crawling and and with lighthouse
+
+```bash
+node cli.js --url https://atom.io/
+
+```
--- cli.js
+++ cli.js
@@ -3,7 +3,7 @@
 const yargs = require('yargs')
 const a11ycrawler = require('.')
 
-const options = yargs.demandOption(['c', 'u'])
+const options = yargs.demandOption(['u'])
   .alias('u', 'url').describe('url', 'URL to crawl')
   .alias('h', 'help').help('h')
   .argv
